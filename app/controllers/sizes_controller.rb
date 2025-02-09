877becf80ac8563559998b4fe3fc21084520c476
Ash Angell <ash@ashleyangell.com>
2025-02-09 15:22:41 +1000

    My changes

--- app/controllers/sizes_controller.rb
+++ app/controllers/sizes_controller.rb
@@ -12,4 +12,32 @@ class SizesController < ApplicationController
     render json: serialize(size, { include: [:item] })
   end
 
+  def create
+    size = item.sizes.build(size_params)
+
+    if size.save
+      render json: serialize(size, { include: [:item] }), status: :created
+    else
+      render json: { errors: size.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if size.update(size_params)
+      render json: serialize(size, { include: [:item] }), status: :ok
+    else
+      render json: { errors: size.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    size.destroy
+    head :no_content
+  end
+
+  private
+
+  def size_params
+    params.require(:size).permit(:name, :value, :unit)
+  end
 end
