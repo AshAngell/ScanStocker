ce765064d7b7f5c9471e5a711a488029b69f39dc
Ash Angell <ash.angell@hey.com>
2025-02-23 11:00:21 +1000

    Add cart_url to serialiser

--- app/serializers/size_serializer.rb
+++ app/serializers/size_serializer.rb
@@ -1,7 +1,12 @@
 class SizeSerializer
   include JSONAPI::Serializer
+  singleton_class.include Rails.application.routes.url_helpers
 
   belongs_to :item
 
   attributes :amount
+
+  link :cart_url do |size|
+    cart_items_item_size_path(size.item, size)
+  end
 end
